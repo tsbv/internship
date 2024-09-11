96091b52f66c22cdd40315dfb06423218cee30fd
Georgij Tsabiev <gtsabiev@inbox.ru>
2024-09-11 22:51:02 +0300

    Реализует затемнение при открытии бургер меню

--- source/js/burger-menu.js
+++ source/js/burger-menu.js
@@ -1,8 +1,16 @@
 const navEl = document.querySelector('.nav');
 const navToggleEl = document.querySelector('.header__toggle');
 const toggleBurgerHandler = () => {
+  const overlay = document.createElement('div');
+  overlay.classList.add('overlay');
   navToggleEl.addEventListener('click', () => {
     navEl.classList.toggle('nav--open');
+    overlay.style.display = navEl.classList.contains('nav--open') ? 'block' : 'none';
+    if (navEl.classList.contains('nav--open')) {
+      document.body.appendChild(overlay);
+    } else {
+      overlay.remove();
+    }
   });
 };
 
