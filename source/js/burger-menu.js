15a1806ca3e70de49e32c8626cf83bebe011c63a
Georgij Tsabiev <gtsabiev@inbox.ru>
2024-09-28 23:34:41 +0300

    Реализует закрытие бургер меню по Esc

--- source/js/burger-menu.js
+++ source/js/burger-menu.js
@@ -8,8 +8,10 @@ const toggleBurgerHandler = () => {
     if (isOpen) {
       overlay.style.display = 'block';
       document.body.appendChild(overlay);
+      document.addEventListener('keydown', handleEscKey);
     } else {
       overlay.remove();
+      document.removeEventListener('keydown', handleEscKey);
     }
   };
   const handleClickOutside = (event) => {
@@ -18,6 +20,12 @@ const toggleBurgerHandler = () => {
       document.removeEventListener('click', handleClickOutside);
     }
   };
+  function handleEscKey (event) { // Function declaration для "поднятия" и нет ошибок.
+    if (event.key === 'Escape') {
+      toggleNav(false);
+      document.removeEventListener('click', handleClickOutside);
+    }
+  }
   navToggleEl.addEventListener('click', () => {
     const isOpen = !navEl.classList.contains('nav--opened');
     toggleNav(isOpen);
