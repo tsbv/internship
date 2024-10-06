8ff877f0d913f43597c4024ca1cd49240f32482d
Georgij Tsabiev <gtsabiev@inbox.ru>
2024-10-07 02:12:58 +0300

    Удаляет лишние комментарии

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,3 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
-
 import { toggleBurgerHandler } from './burger-menu';
 import { setupSelectHandler } from './select';
 import { accordionHandler } from './accordion';
